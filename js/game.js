b801fd3742b9fac1cfc17037ff359c45a18c60da
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2023-12-01 19:50:33 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -15,11 +15,11 @@ var started = false;
 
 var level = 0;
 
-//6. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+
 $(document).keypress(function() {
   if (!started) {
 
-    //7. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    
     $("#level-title2").text("Level " + level);
     nextSequence();
     started = true;
@@ -28,38 +28,38 @@ $(document).keypress(function() {
 
 
 
-//8. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+
 $(".btn").click(function() {
 
-  //9. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  
   var userChosenColour = $(this).attr("id");
 
-  //10. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
 
   animatePress(userChosenColour);
 
-  //console.log(userClickedPattern);
+  
 
-  //11. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
+ 
   checkAnswer(userClickedPattern.length-1);
 });
 
 
-//12. Create a new function called checkAnswer(), it should take one input with the name currentLevel
+
 function checkAnswer(currentLevel) {
 
-    //13. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+   
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
-      // console.log("success");
+     
 
-      //14. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+     
       if (userClickedPattern.length === gamePattern.length){
 
-        //15. Call nextSequence() after a 1000 millisecond delay.
+       
         setTimeout(function () {
             nextSequence();
           }, 1000);
