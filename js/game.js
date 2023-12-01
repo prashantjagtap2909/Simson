e2cd056ab982b6106261fe55f931da42f78f2e7d
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2023-12-01 19:58:46 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,24 +1,16 @@
 
-
-
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 
 var gamePattern = [];
 
-
 var userClickedPattern = [];
-
 
 var started = false;
 
-
 var level = 0;
-
 
 $(document).keypress(function() {
   if (!started) {
-
     
     $("#level-title2").text("Level " + level);
     nextSequence();
@@ -27,13 +19,9 @@ $(document).keypress(function() {
 });
 
 
-
-
 $(".btn").click(function() {
 
-  
   var userChosenColour = $(this).attr("id");
-
 
   userClickedPattern.push(userChosenColour);
 
@@ -68,21 +56,19 @@ function checkAnswer(currentLevel) {
 
       } else {
 
-        // console.log("wrong");
-
-        //16. In the sounds folder, there is a sound called wrong.mp3, play this sound if the user got one of the answers wrong.
+      
+      
         playSound("wrong");
 
-        //17. In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds.
+       
         $("body").addClass("game-over");
         setTimeout(function () {
           $("body").removeClass("game-over");
         }, 200);
 
-        //18. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
         $("#level-title2").text("Game Over, Press Any Keyboard Key to Restart");
 
-        //19. Call startOver() if the user gets the sequence wrong.
+       
         startOver();
 
       }
@@ -90,21 +76,20 @@ function checkAnswer(currentLevel) {
 }
 
 
-//20. Inside game.js create a new function called nextSequence()
+
 function nextSequence() {
 
-  //21. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+
   userClickedPattern = [];
 
-  //22. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
   level++;
 
-  //23. Inside nextSequence(), update the h1 with this change in the value of level.
+  
   $("#level-title2").text("Level " + level);
 
 
 
-  //24. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
+
   var randomNumber = Math.floor(Math.random() * 4);
 
  
@@ -115,14 +100,6 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-/*
-
- 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
-
-}
-*/
 
  
   playSound(randomChosenColour);
